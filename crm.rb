3e1e6bcb5be87d4b9d7a98dd09545845ee777cc6
Moe <kenol.maurice@gmail.com>
2014-06-10 11:55:35 -0400

    contact class

--- crm.rb
+++ crm.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
+require_relative 'contact'
 
-get '/' do |
-	|erb :index
+get '/' do 
+	@crm_app_name = "My CRM"
+	erb :index
 	
 end
+
+
+#new contact
+get '/contacts/new' do
+end
+
+#view a contact
+get '/contacts/:id'do
+end
+
+
+get '/contacts' do
+end
+
+
