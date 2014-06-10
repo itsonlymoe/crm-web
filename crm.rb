3402020a98490991f727685657ec7e7ed077e952
Moe <kenol.maurice@gmail.com>
2014-06-10 12:39:36 -0400

    Add new contact phase 1

--- crm.rb
+++ crm.rb
@@ -1,6 +1,8 @@
-require 'sinatra'
 require_relative 'rolodex'
 require_relative 'Contact'
+require 'sinatra'
+
+@@rolodex = Rolodex.new
 
 get '/' do 
 	@crm_app_name = "My CRM"
@@ -20,11 +22,6 @@ end
 
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-
   erb :contacts
 end
 
