570a7f37d52a33cefe8059f453c2c744e41aa7fe
Moe <kenol.maurice@gmail.com>
2014-06-16 18:28:57 -0400

    refactoring

--- crm.rb
+++ crm.rb
@@ -1,36 +1,31 @@
 require_relative 'rolodex'
-require_relative 'Contact'
+require_relative 'contact'
 require 'sinatra'
 
 @@rolodex = Rolodex.new
-@@crm_app_name = "My CRM"
 
 get '/' do 
-	@@crm_app_name = "My CRM"
+	@crm_app_name = "My CRM"
 	erb :index
 end
 
-
-#new contact
-get '/contacts/new' do
-	@@crm_app_name = "My CRM"
-  erb :new_contact
-end
-
-#view a contact
-get '/contacts/:id'do
-end
-
-
 get '/contacts' do
-  erb :contacts
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  	@@rolodex.add_contact(new_contact)
+  	redirect to('/contacts')
+end 
+
+
+get '/contacts/new' do
+	erb :new_contact
+end
+
+get '/contacts/:id' do
+end
+
+get '/contacts/:id/edit' do
 end
 
 post '/contacts' do
-	contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-	@@rolodex.add_contact(contact)
-	#not rendering a page yet, we can do a redirect back to page
-	redirect('/contacts')
+	puts params
 end
-
-
