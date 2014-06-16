bcfafb0302306afc4f84bb3c08c238d46cd98341
Moe <kenol.maurice@gmail.com>
2014-06-16 19:05:01 -0400

    displaying contacts

--- crm.rb
+++ crm.rb
@@ -3,6 +3,10 @@ require_relative 'contact'
 require 'sinatra'
 
 @@rolodex = Rolodex.new
+contact = @@rolodex.find(1000)
+
+@@rolodex.add_contact(Contact.new("Moe", "johnsn", "moe@gmail.com", "nitesss",))
+
 
 get '/' do 
 	@crm_app_name = "My CRM"
@@ -12,9 +16,13 @@ end
 get '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   	@@rolodex.add_contact(new_contact)
-  	redirect to('/contacts')
+  	redirect to('/')
 end 
 
+get '/contacts/1000' do
+	@contact = @@rolodex.find(1000)
+	erb :show_contact
+end
 
 get '/contacts/new' do
 	erb :new_contact
@@ -29,3 +37,4 @@ end
 post '/contacts' do
 	puts params
 end
+
